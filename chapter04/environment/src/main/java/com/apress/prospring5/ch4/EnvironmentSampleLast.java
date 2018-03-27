af1516913e07439109cbb4656c2177632a2e6e4e
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2018-03-28 00:04:15 +0300

    120 added chnage for the environment example

--- chapter04/environment/src/main/java/com/apress/prospring5/ch4/EnvironmentSampleLast.java
+++ chapter04/environment/src/main/java/com/apress/prospring5/ch4/EnvironmentSampleLast.java
@@ -1,14 +1,15 @@
 package com.apress.prospring5.ch4;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.MutablePropertySources;
 
-public class EnvironmentSample {
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnvironmentSampleLast {
+
     public static void main(String... args) {
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
         ctx.refresh();
@@ -19,13 +20,15 @@ public class EnvironmentSample {
         Map<String,Object> appMap = new HashMap<>();
         appMap.put("application.home", "application_home");
 
-        propertySources.addFirst(new MapPropertySource("prospring5_MAP", appMap));
+        propertySources.addLast(new MapPropertySource("prospring5_MAP", appMap));
 
         System.out.println("user.home: " + System.getProperty("user.home"));
         System.out.println("JAVA_HOME: " + System.getenv("JAVA_HOME"));
 
         System.out.println("user.home: " + env.getProperty("user.home"));
         System.out.println("JAVA_HOME: " + env.getProperty("JAVA_HOME"));
+
+        System.out.println("application.home: " + env.getProperty("application.home"));
 
         ctx.close();
     }
